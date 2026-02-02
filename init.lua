d0ec504fe02ab516d9c6763abc9b66508eaf1189
Claudio Augusto Pereira Rolim <claudioaugusto44@gmail.com>
2026-02-02 09:26:45 -0300

    feat (neo-tree): use floating window and enable relative numbers
    - Set neo-tree window.position to float
    - Enable relativenumber on neo_tree_buffer_enter

--- init.lua
+++ init.lua
@@ -1024,21 +1024,35 @@ require('lazy').setup({
       -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     lazy = false, -- neo-tree will lazily load itself
-    config = function()
-      require('neo-tree').setup {
-        filesystem = {
-          -- Make neo-tree group empty directories like java/com/example
-          group_empty_dirs = true,
-          scan_mode = 'deep',
+
+    keys = {
+      { '<leader>e', '<Cmd>Neotree reveal<CR>', desc = 'Neo-tree (float)' },
+    },
+
+    opts = {
+      window = {
+        position = 'float',
+      },
+      filesystem = {
+        -- Make neo-tree group empty directories like java/com/example
+        group_empty_dirs = true,
+        scan_mode = 'deep',
+      },
+      event_handlers = {
+        {
+          event = 'neo_tree_buffer_enter',
+          -- Isso abaixo é para aparecer relativenumber na janela
+          -- do Neovim também
+          handler = function()
+            vim.wo.number = true
+            vim.wo.relativenumber = true
+          end,
         },
-      }
-    end,
+      },
+    },
+
     ---@module "neo-tree"
     ---@type neotree.Config?
-    opts = {
-      -- fill any relevant options here
-      vim.keymap.set('n', '<leader>e', '<Cmd>Neotree reveal<CR>'),
-    },
   },
   -- diffview.nvim plugin instalation:
   {
